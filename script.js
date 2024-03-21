7fbc7865b891f549072f8937bcf35e9b3a2c9146
Mikaela <19698567+mikaelabelle@users.noreply.github.com>
2024-03-21 17:26:36 +1000

    more logic, still some problems

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let buttons = document.querySelectorAll(".number")
+let inputs = document.querySelectorAll(".input")
 let screen = document.querySelector(".screen")
+let equals = document.querySelector(".equals")
 
 function add(a, b) {
     return a + b
@@ -36,25 +37,80 @@ function operate(numA, operator, numB) {
     }
 }
 
-let currNum = []
+let firstNum = []
+let secondNum = []
+let operation
+let result
+let operations = ["plus", "minus", "multiply", "divide"]
 
-function updateDisplay(keyPressed) {
-    input = keyPressed.textContent
-    if (input != "AC") {
-        currNum.push(input)
-        screen.textContent = currNum.join("")
-    }
+// TODO: problem when pressing second Num twice
 
-    else {
-        currNum = []
-        screen.textContent = ""
-    }
+
+function updateFirstNum(keyPressed) {
+    let input = keyPressed.textContent
+    firstNum.push(input)
+    screen.textContent = firstNum.join("")
 }
 
-buttons.forEach(button => {
-    button.addEventListener("click", event => {
-        updateDisplay(button)
+function updateSecondNum(keyPressed) {
+    let input = keyPressed.textContent
+    secondNum.push(input)
+    screen.textContent = secondNum.join("")
+}
+
+function clearDisplay() {
+    firstNum = []
+    secondNum = []
+    operation = null
+    result = null
+    screen.textContent = ""
+}
+
+function operationSelected(keyPressed) {
+    if (firstNum.length === 0 || secondNum.length === 0) {
+        screen.textContent = ""
+    }
+    operation = keyPressed.id
+}
+
+function getResult(firstNum, secondNum, operation) {
+    return operate(Number(firstNum.join("")), operation, Number(secondNum.join("")))
+}
+
+inputs.forEach(button => {
+    button.addEventListener("click", e => {
+        if (button.textContent === "AC") {
+            clearDisplay()
+        }
+        else if (operations.includes(button.id)) {
+            if (firstNum.length != 0 && secondNum.length != 0) {
+                result = getResult(firstNum, secondNum, operation)
+                screen.textContent = result
+                firstNum = [result]
+
+            }
+
+            operationSelected(button)
+            secondNum = []
+
+        }
+        else if (!operation && firstNum.length === 0) {
+            updateFirstNum(button)
+        }
+        else if (!operations.includes(button.id)) {
+            updateSecondNum(button)
+        }
+        else {
+            result = getResult(firstNum, secondNum, operation)
+            firstNum = result
+            updateSecondNum(button)
+        }
+        console.log({ operation, firstNum, secondNum, result })
     })
 });
 
-console.log(operate(8, "divide", 4))
+equals.addEventListener("click", e => {
+    result = getResult(firstNum, secondNum, operation)
+    operation = null
+    screen.textContent = result
+});
